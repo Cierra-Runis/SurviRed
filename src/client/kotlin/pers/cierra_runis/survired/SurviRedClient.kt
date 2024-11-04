a52826bb037bde1b81d024b4d4940dff80e4ddc6
Cierra_Runis <2864283875@qq.com>
2024-11-04 21:51:57 +0800

    🌞Init

---
+++ src/client/kotlin/pers/cierra_runis/survired/SurviRedClient.kt
@@ -0,0 +1,9 @@
+package pers.cierra_runis.survired
+
+import net.fabricmc.api.ClientModInitializer
+
+object SurviRedClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/pers/cierra_runis/survired/SurviRedDataGenerator.kt
@@ -0,0 +1,9 @@
+package pers.cierra_runis.survired
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object SurviRedDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
