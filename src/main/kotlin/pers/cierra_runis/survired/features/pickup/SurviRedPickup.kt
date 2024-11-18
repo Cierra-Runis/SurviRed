aec349df020952c3f061bbc92d685c3797c420dd
Cierra_Runis <2864283875@qq.com>
2024-11-18 20:51:40 +0800

    Fix Feature `Pickup`

--- src/main/kotlin/pers/cierra_runis/survired/features/pickup/SurviRedPickup.kt
+++ src/main/kotlin/pers/cierra_runis/survired/features/pickup/SurviRedPickup.kt
@@ -1,4 +1,4 @@
-package pers.cierra_runis.survired.pickup
+package pers.cierra_runis.survired.features.pickup
 
 import net.fabricmc.fabric.api.event.player.UseEntityCallback
 import net.minecraft.entity.Entity
@@ -6,12 +6,10 @@ import net.minecraft.item.ItemStack
 import net.minecraft.item.SpawnEggItem
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.util.ActionResult
-import org.slf4j.LoggerFactory
+import pers.cierra_runis.survired.common.SurviRedRegister
 
-object SurviRedPickupEventListener {
-    private val logger = LoggerFactory.getLogger(this.javaClass)
-
-    fun registerRightClickEvent() {
+object SurviRedPickup : SurviRedRegister {
+    override fun register() {
         UseEntityCallback.EVENT.register(UseEntityCallback { playerEntity, world, hand, entity, entityHitResult ->
             if (!playerEntity.isSneaking) return@UseEntityCallback ActionResult.PASS
 
@@ -21,7 +19,7 @@ object SurviRedPickupEventListener {
             for (key in arrayOf("Pos", "Motion", "Rotation", "UUID")) entityNbt.remove(key)
 
             val stack = ItemStack(spawnEgg).apply {
-                NbtCompound().apply {
+                nbt = NbtCompound().apply {
                     put("EntityTag", entityNbt)
                 }
             }
